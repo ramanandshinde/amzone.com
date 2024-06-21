c0bc39c1b3dbbf4bea5289844e0dca88d415acab
ramanandshinde <160994126+ramanandshinde@users.noreply.github.com>
2024-06-21 22:37:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const country="India";
+const img=" url(https://hips.hearstapps.com/hmg-prod/images/interior-design-ideas-idea-9-dwell-1-1644331792.jpg?crop=1xw:1xh;center,top&resize=980:*);"
